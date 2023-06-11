cc09609ca15eee363172f70f1f6fc269d8bf0eeb
Tanuj-Kandpal <108190243+Tanuj-Kandpal@users.noreply.github.com>
2023-06-11 17:02:45 +0530

    GSAP implemented

---
+++ script.js
@@ -0,0 +1,16 @@
+gsap.from(".coverpic", {
+  duration: 1,
+  opacity: 0,
+});
+gsap.to(".coverpic", {
+  duration: 4,
+  opacity: 1,
+});
+gsap.from(".frame", {
+  duration: 1,
+  opacity: 0,
+});
+gsap.to(".frame", {
+  duration: 4,
+  opacity: 1,
+});
